c9f86f7f89a3b8c33a2aa8ac89b3179a8a285f8e
Kieren Evans <kierenevans@users.noreply.github.com>
2019-02-13 10:58:21 +0200

    PHP 5.6/7.1/7.2 Support

--- docker/image/console/root/lib/task/skeleton/apply.sh
+++ docker/image/console/root/lib/task/skeleton/apply.sh
@@ -2,5 +2,10 @@
 
 function task_skeleton_apply()
 {
+    if [ "${PHP_VERSION:0:3}" == "5.6" ]; then
+      mv /home/build/skeleton/composer-5.6.json /home/build/skeleton/composer.json
+    else
+      rm -f /home/build/skeleton/composer-5.6.json
+    fi
     run "rsync --exclude='*.twig' --ignore-existing -a /home/build/skeleton/ /app/"
 }
