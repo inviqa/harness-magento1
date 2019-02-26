8efe8fdba421d44e86a50ea1882654e6e7d5a108
Kieren Evans <kevans@inviqa.com>
2019-02-26 23:13:37 +0200

    Support alternative assets directories

--- docker/image/console/root/lib/task/assets/apply.sh
+++ docker/image/console/root/lib/task/assets/apply.sh
@@ -2,13 +2,15 @@
 
 function task_assets_apply()
 {
+    local ASSETS_DIR="${ASSETS_DIR:-tools/assets/development}"
+
     SQL="SELECT IF (COUNT(*) = 0, 'no', 'yes') FROM information_schema.tables WHERE table_schema = '$DB_NAME';"
     IS_DATABASE_APPLIED="$(mysql -ss -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -e "$SQL")"
 
     if [ "$IS_DATABASE_APPLIED" = "no" ]; then
 
-        if [ -f "/app/tools/assets/development/${DB_NAME}.sql.gz" ]; then
-            run "zcat /app/tools/assets/development/${DB_NAME}.sql.gz | mysql -h $DB_HOST -u root -p$DB_ROOT_PASS $DB_NAME"
+        if [ -f "/app/${ASSETS_DIR}/${DB_NAME}.sql.gz" ]; then
+            run "zcat /app/${ASSETS_DIR}/${DB_NAME}.sql.gz | mysql -h $DB_HOST -u root -p$DB_ROOT_PASS $DB_NAME"
         else
             task "magento:install"
         fi
